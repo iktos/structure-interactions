e1f02fec281a9a38465a0c64f03db70a71e9048f
Yanyuan Zhang <yanyuan.zhang@iktos.com>
2022-03-09 10:51:46 +0100

    remove iktos licences

--- iktos/structure_interactions/plip/utils.py
+++ iktos/structure_interactions/plip/utils.py
@@ -1,9 +1,3 @@
-"""
-Copyright (C) Iktos - All Rights Reserved
-Unauthorized copying of this file, via any medium is strictly prohibited
-Proprietary and confidential
-"""
-
 import os
 from pathlib import Path
 from typing import Optional
--- iktos/your_package_name/__init__.py
+++
@@ -1,3 +0,0 @@
-def func():
-    # You import this function with `from iktos.your_package_name import func`
-    pass
