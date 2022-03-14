d706742f129af811f5af69804457ab8b05ca7392
Yanyuan Zhang <yanyuan.zhang@iktos.com>
2022-03-14 14:19:42 +0100

    test: unlist 'contacts_ref'

--- tests/test_InteractionProfiler.py
+++ tests/test_InteractionProfiler.py
@@ -6,21 +6,19 @@ def test_analyse_complex_3S3M():
     ligand_path = 'tests/data/lig_3S3M.sdf'
 
     plip = InteractionProfiler(plip_config_version='default')  # set plip_config here
-    contacts_ref = [
-        plip.analyse_complex(
-            rec_coords=protein_path,
-            lig_coords=ligand_path,
-            as_string=False,
-            refine=True,
-            lig_format='sdf',
-        )
-    ]
+    contacts_ref = plip.analyse_complex(
+        rec_coords=protein_path,
+        lig_coords=ligand_path,
+        as_string=False,
+        refine=True,
+        lig_format='sdf',
+    )
 
-    assert len(contacts_ref[0]["Hydrophobic"]) == 2
-    assert len(contacts_ref[0]["Pi_Stacking"]) == 3
-    assert len(contacts_ref[0]["Pi_Hydrophobic"]) == 1
-    assert len(contacts_ref[0]["Metal_Complex"]) == 2
-    assert len(contacts_ref[0]["Water_Bridge"]) == 1
+    assert len(contacts_ref["Hydrophobic"]) == 2
+    assert len(contacts_ref["Pi_Stacking"]) == 3
+    assert len(contacts_ref["Pi_Hydrophobic"]) == 1
+    assert len(contacts_ref["Metal_Complex"]) == 2
+    assert len(contacts_ref["Water_Bridge"]) == 1
 
 
 def test_analyse_complex_5N9T():
@@ -29,18 +27,16 @@ def test_analyse_complex_5N9T():
     ligand_path = 'tests/data/lig_5N9T.sdf'
 
     plip = InteractionProfiler(plip_config_version='default')  # set plip_config here
-    contacts_ref = [
-        plip.analyse_complex(
-            rec_coords=protein_path,
-            lig_coords=ligand_path,
-            as_string=False,
-            refine=True,
-            lig_format='sdf',
-        )
-    ]
+    contacts_ref = plip.analyse_complex(
+        rec_coords=protein_path,
+        lig_coords=ligand_path,
+        as_string=False,
+        refine=True,
+        lig_format='sdf',
+    )
 
-    assert len(contacts_ref[0]["Hydrophobic"]) == 7
-    assert len(contacts_ref[0]["Pi_Hydrophobic"]) == 2
-    assert len(contacts_ref[0]["H_Bond"]) == 5
-    assert len(contacts_ref[0]["H_Bond_Weak"]) == 2
-    assert len(contacts_ref[0]["Multipolar"]) == 3
+    assert len(contacts_ref["Hydrophobic"]) == 7
+    assert len(contacts_ref["Pi_Hydrophobic"]) == 2
+    assert len(contacts_ref["H_Bond"]) == 5
+    assert len(contacts_ref["H_Bond_Weak"]) == 2
+    assert len(contacts_ref["Multipolar"]) == 3
