ab4e6ea6691210e575374184ec367d7ccf300d73
Stéphanie Labouille <stephanie.labouille@iktos.com>
2022-05-19 12:40:10 +0000

    Resolve "Add `analyse_complex` and `analyse_complexes` in __init__"

--- iktos/structure_interactions/__init__.py
+++ iktos/structure_interactions/__init__.py
@@ -1,4 +1,10 @@
+from .analyse_complex import analyse_complex, analyse_complexes
 from .Atom import Atom
 from .InteractionProfiler import InteractionProfiler
 
-__all__ = ['Atom', 'InteractionProfiler']
+__all__ = [
+    'analyse_complex',
+    'analyse_complexes',
+    'Atom',
+    'InteractionProfiler',
+]
