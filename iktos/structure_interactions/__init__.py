d84af0683c15c115696b612b46f58632a05a8512
Yanyuan Zhang <yanyuan.zhang@iktos.com>
2022-05-19 13:52:28 +0000

    Resolve "Create test for analyse_complexes"

--- iktos/structure_interactions/__init__.py
+++ iktos/structure_interactions/__init__.py
@@ -1,10 +1,12 @@
 from .analyse_complex import analyse_complex, analyse_complexes
 from .Atom import Atom
 from .InteractionProfiler import InteractionProfiler
+from .utils import contacts_to_dict
 
 __all__ = [
     'analyse_complex',
     'analyse_complexes',
     'Atom',
+    'contacts_to_dict',
     'InteractionProfiler',
 ]
