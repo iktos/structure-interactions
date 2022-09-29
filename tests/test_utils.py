87afcfa5c97b4feb08472aca1fce8adbb138568c
Stéphanie Labouille <stephanie.labouille@iktos.com>
2022-09-29 15:45:14 +0000

    Add more tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,7 +6,88 @@ from iktos.structure_interactions.analyse_complex import analyse_complex
 from iktos.structure_interactions.utils import contacts_to_dict
 
 
-def test_contacts_to_dict():
+def test_contacts_to_dict_1P5E():
+    # test that the conversion to dict gives exactly
+    # what is expected in SA - if this test breaks, you should consider
+    # carefully the modifications that are being made to the package
+    # as they will probably compromise the compatibility with SA
+    protein_path = 'tests/data/prot_1P5E.pdb'
+    ligand_path = 'tests/data/lig_1P5E.sdf'
+
+    contacts_ref = analyse_complex(
+        rec_coords=protein_path,
+        lig_coords=ligand_path,
+        as_string=False,
+        refine=True,
+        lig_format='sdf',
+    )
+    contacts_ref = contacts_to_dict(contacts_ref)
+
+    with open('tests/data/contacts_1P5E.json', 'r') as f:
+        expected_contacts = json.load(f)
+    assert not DeepDiff(
+        contacts_ref,
+        expected_contacts,
+        ignore_order=True,
+        ignore_numeric_type_changes=True,
+    )
+
+
+def test_contacts_to_dict_3S3M():
+    # test that the conversion to dict gives exactly
+    # what is expected in SA - if this test breaks, you should consider
+    # carefully the modifications that are being made to the package
+    # as they will probably compromise the compatibility with SA
+    protein_path = 'tests/data/prot_3S3M.pdb'
+    ligand_path = 'tests/data/lig_3S3M.sdf'
+
+    contacts_ref = analyse_complex(
+        rec_coords=protein_path,
+        lig_coords=ligand_path,
+        as_string=False,
+        refine=True,
+        lig_format='sdf',
+    )
+    contacts_ref = contacts_to_dict(contacts_ref)
+
+    with open('tests/data/contacts_3S3M.json', 'r') as f:
+        expected_contacts = json.load(f)
+    assert not DeepDiff(
+        contacts_ref,
+        expected_contacts,
+        ignore_order=True,
+        ignore_numeric_type_changes=True,
+    )
+
+
+def test_contacts_to_dict_5N9T():
+    # test that the conversion to dict gives exactly
+    # what is expected in SA - if this test breaks, you should consider
+    # carefully the modifications that are being made to the package
+    # as they will probably compromise the compatibility with SA
+    protein_path = 'tests/data/prot_5N9T.pdb'
+    ligand_path = 'tests/data/lig_5N9T.sdf'
+
+    contacts_ref = analyse_complex(
+        rec_coords=protein_path,
+        lig_coords=ligand_path,
+        as_string=False,
+        refine=True,
+        lig_format='sdf',
+    )
+    contacts_ref = contacts_to_dict(contacts_ref)
+
+    with open('tests/data/contacts_5N9T.json', 'r') as f:
+        expected_contacts = json.load(f)
+    assert not DeepDiff(
+        contacts_ref,
+        expected_contacts,
+        ignore_order=True,
+        ignore_numeric_type_changes=True,
+    )
+
+
+def test_contacts_to_dict_5UIT():
     # test that the conversion to dict gives exactly
     # what is expected in SA - if this test breaks, you should consider
     # carefully the modifications that are being made to the package
@@ -25,4 +106,63 @@ def test_contacts_to_dict():
 
     with open('tests/data/contacts_5UIT.json', 'r') as f:
         expected_contacts = json.load(f)
-    assert not DeepDiff(contacts_ref, expected_contacts, ignore_order=True)
+    assert not DeepDiff(
+        contacts_ref,
+        expected_contacts,
+        ignore_order=True,
+        ignore_numeric_type_changes=True,
+    )
+
+
+def test_contacts_to_dict_6NO9():
+    # test that the conversion to dict gives exactly
+    # what is expected in SA - if this test breaks, you should consider
+    # carefully the modifications that are being made to the package
+    # as they will probably compromise the compatibility with SA
+    protein_path = 'tests/data/prot_6NO9.pdb'
+    ligand_path = 'tests/data/lig_6NO9.sdf'
+
+    contacts_ref = analyse_complex(
+        rec_coords=protein_path,
+        lig_coords=ligand_path,
+        as_string=False,
+        refine=True,
+        lig_format='sdf',
+    )
+    contacts_ref = contacts_to_dict(contacts_ref)
+
+    with open('tests/data/contacts_6NO9.json', 'r') as f:
+        expected_contacts = json.load(f)
+    assert not DeepDiff(
+        contacts_ref,
+        expected_contacts,
+        ignore_order=True,
+        ignore_numeric_type_changes=True,
+    )
+
+
+def test_contacts_to_dict_6NW6():
+    # test that the conversion to dict gives exactly
+    # what is expected in SA - if this test breaks, you should consider
+    # carefully the modifications that are being made to the package
+    # as they will probably compromise the compatibility with SA
+    protein_path = 'tests/data/prot_6NW6.pdb'
+    ligand_path = 'tests/data/lig_6NW6.sdf'
+
+    contacts_ref = analyse_complex(
+        rec_coords=protein_path,
+        lig_coords=ligand_path,
+        as_string=False,
+        refine=True,
+        lig_format='sdf',
+    )
+    contacts_ref = contacts_to_dict(contacts_ref)
+
+    with open('tests/data/contacts_6NW6.json', 'r') as f:
+        expected_contacts = json.load(f)
+    assert not DeepDiff(
+        contacts_ref,
+        expected_contacts,
+        ignore_order=True,
+        ignore_numeric_type_changes=True,
+    )
